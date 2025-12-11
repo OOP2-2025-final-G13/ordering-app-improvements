244d0aa886bbf445de37afeb94aa30b6f4cacb1c
浅野僚介 <ruidora1529@gmail.com>
2025-12-11 14:44:17 +0900

    ユーザー情報に住所を追加
    ユーザー情報に住所を追加できるようにし、編集もできるようにした

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ from .db import db
 class User(Model):
     name = CharField()
     age = IntegerField()
+    address = CharField()
 
     class Meta:
         database = db
--- routes/user.py
+++ routes/user.py
@@ -20,7 +20,8 @@ def add():
     if request.method == 'POST':
         name = request.form['name']
         age = request.form['age']
-        User.create(name=name, age=age)
+        address = request.form['address']
+        User.create(name=name, age=age, address=address)
         return redirect(url_for('user.list'))
     
     return render_template('user_add.html')
@@ -35,6 +36,7 @@ def edit(user_id):
     if request.method == 'POST':
         user.name = request.form['name']
         user.age = request.form['age']
+        user.address = request.form['address']
         user.save()
         return redirect(url_for('user.list'))
 
